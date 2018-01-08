e9d58f31a15c198872ceee7b8cfe6083a18b7ab3
Yuri47 <yuri.alexs@gmail.com>
2018-01-08 18:12:13 -0200

    start product page

---
+++ Template HTML/js/app.js
@@ -0,0 +1,69 @@
+        
+         var tamanho =  $(window).width(); 
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        if (tamanho >= 751) {
+       
+          $(".menu-rosa").ready(function() {
+  
+  
+     jQuery(window).scroll(function() {
+        var windowScrollPosTop = jQuery(window).scrollTop();
+
+        if(windowScrollPosTop >= 163) {
+
+            
+          jQuery("nav").addClass( "fixed-top" );
+           jQuery("#promo").addClass( "espaco" );
+            
+           
+         
+        }
+        else{
+          jQuery("nav").removeClass( "fixed-top" );
+           jQuery("#promo").removeClass( "espaco" );
+             
+        }
+     });
+              
+           
+});
+        } else if(tamanho <= 750) {
+            
+                   $(".menu-rosa").ready(function() {
+  
+  
+     jQuery(window).scroll(function() {
+        var windowScrollPosTop = jQuery(window).scrollTop();
+
+        if(windowScrollPosTop >= 250) {
+
+            
+          jQuery("nav").addClass( "fixed-top" );
+           jQuery("#promo").addClass( "espaco-mobile" );
+            
+           
+         
+        }
+        else{
+          jQuery("nav").removeClass( "fixed-top" );
+           jQuery("#promo").removeClass( "espaco-mobile" );
+             
+        }
+     });
+              
+           
+});
+            
+        }
