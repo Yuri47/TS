81e47bed774dc0f7efd78555035d2009dfce59a9
Yuri Alexs <yuri.alexs@gmail.com>
2018-01-19 19:40:37 -0200

    Add function theme color

--- Template WP/WP/app/public/wp-content/themes/kplshop/functions.php
+++ Template WP/WP/app/public/wp-content/themes/kplshop/functions.php
@@ -7,7 +7,15 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+add_action('wp_head', 'theme_color', 60);
 
+function theme_color() {
+    
+    ?>
+    <meta name=theme-color content=#ff009d>
+    <?php
+    
+}
 
 
 //wp_enqueue_script( 'yd-jquery', get_stylesheet_directory_uri() . '/assets/js/jquery-3.2.1.slim.min.js', array(), '4.0', true );
